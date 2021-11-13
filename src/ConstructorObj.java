d99fbe5db5e1b89c6504f5e60847a25881ba4828
gagandeep1993 <deepgagan1542@gmail.com>
2021-11-13 12:53:24 -0500

    adding one more programs

---
+++ src/ConstructorObj.java
@@ -0,0 +1,9 @@
+public class ConstructorObj
+{
+    public static void main(String[] args)
+    {
+        ConstructordDemo obj=new  ConstructordDemo();
+        // ......................this above is DEFAULT CONSTRUCTOR.........................
+
+    }
+}
---
+++ src/ConstructordDemo.java
@@ -0,0 +1,11 @@
+public class ConstructordDemo
+{
+    private int id;
+    String name;
+
+    ConstructordDemo()
+    {
+        System.out.println("inside constructor or this is a constructor");
+    }
+
+}
