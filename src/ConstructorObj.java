a3afc1ab4872637986cf2a50d48e6e718eab961a
gagandeep1993 <deepgagan1542@gmail.com>
2021-11-18 20:30:48 -0500

    adding  more programs

--- src/ConstructorObj.java
+++ src/ConstructorObj.java
@@ -2,8 +2,12 @@ public class ConstructorObj
 {
     public static void main(String[] args)
     {
-        ConstructordDemo obj=new  ConstructordDemo();
-        // ......................this above is DEFAULT CONSTRUCTOR.........................
+        ConstructordDemo obj1=new  ConstructordDemo();//constructor
+        ConstructordDemo obj2=new  ConstructordDemo(34,"gagan");
+        ConstructordDemo obj=new  ConstructordDemo(34,"gagan",678);
+        //parameter constructor
+
+         obj2.show();
 
     }
 }
