c33f16a8219e7464ac832423604814e05300c4a1
gagandeep1993 <deepgagan1542@gmail.com>
2022-01-05 14:20:06 -0500

    adding  overloading program

---
+++ src/Dance.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Dance
+{
+    void danceperforming();
+
+}
+class Salsa implements Dance
+{
+    @Override
+    public void danceperforming()
+    {
+        System.out.println("Salaaaaa");
+    }
+}
+class Classical implements Dance
+{
+    @Override
+    public void danceperforming()
+    {
+        System.out.println("Classicallll");
+    }
+}
+
+class Performer
+{
+    public static void main(String[] args)
+    {
+        List<Dance> p = regcounter();
+        for (Dance i : p)
+        {
+            i.danceperforming();
+        }
+    }
+
+        public static List<Dance> regcounter ()
+        {
+            List<Dance> p = new ArrayList();
+            p.add(new Salsa());
+            p.add(new Classical());
+            return p;
+
+        }
+
+}
