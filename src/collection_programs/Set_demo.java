b627ae1c4d575428deb7db47dcd172931ae6db3a
gagandeep1993 <deepgagan1542@gmail.com>
2022-02-09 15:33:30 -0500

    set

---
+++ src/collection_programs/Set_demo.java
@@ -0,0 +1,18 @@
+package collection_programs;
+import java.util.*;
+
+import java.util.HashSet;
+
+public class Set_demo
+{
+  public static void main(String[] args)
+  {
+    HashSet<Integer> s=new HashSet();
+    s.add(9);
+    s.add(3);
+    s.add(384);
+    s.add(37);
+    s.add(3);
+    System.out.println(s);
+  }
+}
